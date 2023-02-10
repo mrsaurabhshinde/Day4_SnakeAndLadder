9b4e4ae62b640b923614617ee3e8364432dc174d
saurabh <mrsaurabhshinde5@gmail.com>
2023-02-10 17:07:44 +0530

    Added game with 2 and player won the game.

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -10,59 +10,97 @@ public class SnakeLadder {
         return randomValue;
     }
 
-    static int getRandomOption(int startgame, int dice) {
+    static int getRandomOption(int startPosition, int dice) {
         int randomOption = random.nextInt(3);
         randomOption = randomOption + 1;
         System.out.println("Choose option : " +randomOption);
 
         switch(randomOption) {
             case 0 :
-                System.out.println("no play then player stay same position");
+                System.out.println("no play");
                 break;
             case 1 :
-                startgame = startgame + dice;
-                System.out.println("Ladder the Player moves ahead :" + startgame);
+                startPosition =startPosition + dice;
+                System.out.println("Ladder the Player moves ahead :" + startPosition);
                 break;
             case 2 :
-                if(startgame >= 0)
-                    startgame = startgame - dice;
-                System.out.println("Snake the player moves behind : " + startgame);
+                if(startPosition >= 0)
+                    startPosition = startPosition - dice;
+                System.out.println("Snake the player moves behind : " + startPosition);
                 break;
             default :
         }
         System.out.println();
-        return startgame;
+        return startPosition;
     }
 
     public static void main(String[] args) {
 
-        int startgame = 0;
+        int startPosition1 = 0;
+        int startPosition2 = 0;
         int dice ;
-        int countDice = 0;
-        int countPosition = 0;
+        int countDice1 = 0;
+        int countPos1 = 0;
+        int countDice2 = 0;
+        int countPos2 = 0;
 
         System.out.println("Welcome to Snake And Ladder Game \n");
-        System.out.println("Single Player at start position " +startgame);
-        while (startgame <= 100)	{
-            dice = getRandomValue();
-            countDice ++;
-            int t = startgame;
-            startgame=getRandomOption(startgame , dice);
-            if(startgame > t)
-                countPosition ++;
+        System.out.println("Player-1 at start position " +startPosition1);
+        System.out.println("Player-2 at start position " +startPosition2);
+
+        while (startPosition1 <= 100 || startPosition2 <= 100)	{
+            Random random = new Random();
+            int play = random.nextInt(2);
+            play = play +1;
+            if (play == 1) {
+                dice = getRandomValue();
+                countDice1 ++;
+                int t = startPosition1;
+                startPosition1=getRandomOption(startPosition1 , dice);
+                if(startPosition1 > t)
+                    countPos1 ++;
+                if(startPosition1 > 100)
+                    break;
+            }
+            else {
+                dice = getRandomValue();
+                countDice2 ++;
+                int t = startPosition2;
+                startPosition2=getRandomOption(startPosition2 , dice);
+                if(startPosition2 > t)
+                    countPos2 ++;
+                if(startPosition2 > 100)
+                    break;
+            }
         }
-        if (startgame > 100) {
-            int temp = startgame - 100;
-            startgame = startgame - temp;
-            System.out.println("win the game position is : "+startgame);
-            System.out.println("Number of count dice : "+countDice);
-            System.out.println("number of count position : "+countPosition);
+        if (startPosition1 > 100 || startPosition2 > 100) {
+            int temp1 = startPosition1 - 100;
+            startPosition1 = startPosition1 - temp1;
+            int temp2 = startPosition2 - 100;
+            startPosition2 = startPosition2 - temp2;
+
+            if (startPosition1 == 100 ) {
+                System.out.println("Player-1 win the game position : "+startPosition1);
+                System.out.println("Player-1 No of count dice : "+countDice1);
+                System.out.println("Player-1 no of count position : "+countPos1);
+            }
+            else if (startPosition2 == 100 ) {
+                System.out.println("Player-2 win the game position : "+startPosition2);
+                System.out.println("Player-2 No of count dice : "+countDice2);
+                System.out.println("Player-2 no of count position : "+countPos2);
+            }
         }
         else {
-            System.out.println("Win the game postion : " +startgame);
-            System.out.println("number of count dice : "+countDice);
-            System.out.println("number of count position : "+countPosition);
+            if (startPosition1 == 100) {
+                System.out.println("Player-1 Win the game postion : " +startPosition1);
+                System.out.println("Player-1 no of count dice : "+countDice1);
+                System.out.println("Player-1 no of count position : "+countPos1);
+            }
+            else {
+                System.out.println("Player-2 Win the game postion : " +startPosition2);
+                System.out.println("Player-2 no of count dice : "+countDice2);
+                System.out.println("Player-2 no of count position : "+countPos2);
+            }
         }
     }
-
 }
